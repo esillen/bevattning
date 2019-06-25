16cd4f03b8faae8db15edb5a680cf727dd453092
Erik Sillén <erik.sillen.1990@gmail.com>
2019-06-25 16:10:17 +0200

    Actually uploading the built frontend to git! Next: have it as a git hook!

---
+++ frontend/app/build/precache-manifest.e6837e3f394041da61116d87932c61ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202413fdfbfd8028d97fe0ad47fd06b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "000fda1af190250c84e4",
+    "url": "/static/css/main.3a9357a5.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "000fda1af190250c84e4",
+    "url": "/static/js/main.a459fa1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "76cf0a92e8fd7ff958aac12b83cf7add",
+    "url": "/static/media/watering.76cf0a92.gif"
+  }
+]);
