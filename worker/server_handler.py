80080d400cec22a0ade8ae2d798af84b7f79bfbe
Erik Sillén <erik.sillen.1990@gmail.com>
2019-06-26 13:40:17 +0200

    Fixed connection issues in worker

--- worker/server_handler.py
+++ worker/server_handler.py
@@ -11,19 +11,31 @@ class ServerHandler:
         self.session.auth = (username, password)
 
     def get_valve_states(self):
-        response = self.session.get(self.host + VALVES)
+        response = None
+        try:
+            response = self.session.get(self.host + VALVES, timeout = 10)
+        except:
+            print ("WARNING WARNING COULD NOT READ FROM SERVER! CLIENT SIDE ERROR! VERY UNCOMFORTABLE")
+            return False
         if response.status_code != 200:
-            print ("WARNING WARNING COULD NOT READ FROM SERVER! VERY UNCOMFORTABLE")
+            print ("WARNING WARNING COULD NOT READ FROM SERVER! BAD RESPONSE CODE FROM SERVER! VERY UNCOMFORTABLE")
             return False
         else:
             data = response.json()
-            valves = data["valves"]
-            valves = dict([(item["id"], item["state"]) for item in valves]) # Returns a dict with (id, state) for the valves
+            try:
+                valves = data["valves"]
+                valves = dict([(item["id"], item["state"]) for item in valves]) # Returns a dict with (id, state) for the valves
+            except:
+                print ("BAD RESPONSE FROM SERVER! SOMETHING MUST HAVE CHANGED IN THE RESPONSE! VERY UNCOMFORTABLE")
+                return False
             return valves
 
     def update_mi_flora_data(self, data):
-        response = self.session.post(self.host + MIFLORA, json = data)
-        if response.status_code != 200:
+        response = None
+        try:
+            response = self.session.post(self.host + MIFLORA, json = data)
+            if response.status_code != 200:
+                print ("WARNING WARNING COULD NOT SEND DATA TO SERVER")
+        except:
             print ("WARNING WARNING COULD NOT SEND DATA TO SERVER")
-
 
